cc4cc807876696fdf0f26b810d37249da9c4ea23
Mamta — <mamta@Mamtas-MBP.lan>
2021-05-14 09:13:28 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Rentals from './components/rentals'
 import MovieForm from './components/movieForm'
 import NotFound from './components/notFound'
 import Customers from './components/customers'
+import LoginForm from './components/loginForm'
+import Register from './components/register'
 import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css'
 
@@ -17,10 +19,12 @@ class App extends Component {
       <NavBar/>
       <main className="container">
       <Switch>
+      <Route path="/login" component={LoginForm} ></Route>
       <Route path="/movies/:id" component={MovieForm}/>
         <Route path="/movies" component={Movies} ></Route>
         <Route path="/customers" component={Customers} ></Route>
         <Route path="/rentals" component={Rentals} ></Route>
+        <Route path="/register" component={Register} ></Route>
         <Route path="/not-found" component={NotFound} ></Route>
         <Redirect from="/" exact to="/movies"></Redirect>
         <Redirect  to="/not-found"></Redirect>
